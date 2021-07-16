e52c3912e386ec72e139defc0fa2d7b8f7e4b39d
유수민 <sumin@yusumin-ui-MacBookPro.local>
2021-07-16 21:26:32 +0900

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase/app'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAeac6C1KmfHw5rFV5L7RyT2CHX83yoLag",
+    authDomain: "portfolio-4ac45.firebaseapp.com",
+    projectId: "portfolio-4ac45",
+    storageBucket: "portfolio-4ac45.appspot.com",
+    messagingSenderId: "337281380561",
+    appId: "1:337281380561:web:21d73a29a8613c9e5695f0",
+    measurementId: "G-088059DX3F"
+  };
+  // Initialize Firebase
+  export default firebase.initializeApp(firebaseConfig);
--- src/index.js
+++ src/index.js
@@ -1,17 +1,46 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+
+import firebase from './firebase.js'
+import "firebase/analytics";
+import "firebase/auth";
+import "firebase/firestore";
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Home from "views/Home.js";
+import Project from "views/Project.js";
+
+// TODO: Replace the following with your app's Firebase project configuration
+// For Firebase JavaScript SDK v7.20.0 and later, `measurementId` is an optional field
+
+//TODO: json 파일 경로 변경 , function 추가
+
+
+console.log(firebase);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  
+  <BrowserRouter>
+    <Switch>
+      <Route 
+        path="/" 
+        exact 
+        render={props => <Home {...props} />} 
+      />
+      <Route 
+        path="/home" 
+        exact 
+        render={props => <Home {...props} />} 
+      />
+      <Route
+        path="/project"
+        exact
+        render={props => <Project {...props} />}
+      />
+      <Redirect to="/" />
+    </Switch>
+  </BrowserRouter>,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
